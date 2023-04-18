70f464d41f975c0e4aa8bd98ff7f6bccc0ac3f0d
Devmangrani <devmangrani5@gmail.com>
2023-04-18 13:25:23 +0530

    Day2 commit

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -1,6 +1,27 @@
-public class Day2{
+import java.util.Scanner;
+
+public class Day2 {
     public static void main(String args[]) {
-        System.out.println("Day2");   
+        //Cheak if a number is prime or not?
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==2) {
+            System.out.println("n is prime");
+        }else {
+            boolean isPrime = true;
+        for(int i=2; i<=n-1; i++){
+            if(n % i == 0){ //n is a multiple of i 
+                isPrime = false;
+            }
+        }
+        if (isPrime == true) {
+            System.out.println("n is Prime no. ");
+        }else{
+            System.out.println("n is not a Prime no.");
+        }
+        }
+        
+           
     }
 
 }
