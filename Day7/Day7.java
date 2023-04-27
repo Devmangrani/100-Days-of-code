9d3cf74540789d59383309f3a97195cb5afa0446
Devmangrani <devmangrani5@gmail.com>
2023-04-27 22:54:25 +0530

    Day7 commit

---
+++ Day7/Day7.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Day7 {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Input the first number: ");
+double x = sc.nextDouble();
+System.out.print("Input the second number: ");
+double y = sc.nextDouble();
+System.out.print("Input the third number: ");
+double z = sc.nextDouble();
+System.out.print("The average value is " + average(x, y, z)+"\n" );
+}
+public static double average(double x, double y, double z) {
+return (x + y + z) / 3;
+}
+}
