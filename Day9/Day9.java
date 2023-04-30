2ceb4e6637ef20e2e408ed9e918bd3a9b77f67e3
Devmangrani <devmangrani5@gmail.com>
2023-04-30 15:03:24 +0530

    Day9 Commit

---
+++ Day9/Day9.java
@@ -0,0 +1,12 @@
+//package 100 Days of code.Day9;
+
+public class Day9 {
+    public static void main(String args[]) {
+        int n = 1;
+        while(n <= 100) {
+            System.out.println(n);
+            n++;
+        }
+        System.out.println();
+}
+}
