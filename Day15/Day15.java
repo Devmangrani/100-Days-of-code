c8bf3ef9379849bd63a291223c871b236f6b464c
Devmangrani <devmangrani5@gmail.com>
2023-05-05 23:43:32 +0530

    Day15 commit

---
+++ Day15/Day15.java
@@ -0,0 +1,17 @@
+//package 100 Days of code.Day15;
+
+public class Day15 {
+    public static void pairs(int number[]) {
+        for (int i = 0; i<number.length; i++) {
+            int curr = number[i];
+            for (int j = i+1; j < number.length; j++) {
+                System.out.print("(" + curr + "," + number[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int number[] = {2,4,6,8,10};
+        pairs(number);
+    }
+}
