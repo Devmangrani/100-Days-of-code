6902b2efa9df113cd2054d51b63190f7ba17fdc2
Devmangrani <devmangrani5@gmail.com>
2023-05-10 23:12:54 +0530

    Day20 Commit

---
+++ Day20/Day20.java
@@ -0,0 +1,9 @@
+public class Day20 {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int num:nums){
+            xor = xor^num;
+        }
+        return xor;
+    }
+}
