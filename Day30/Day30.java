7f6a7ebdab452e24644f1fb35911302aa04a1c1f
Devmangrani <devmangrani5@gmail.com>
2023-05-24 21:54:44 +0530

    Day30 commit

---
+++ Day30/Day30.java
@@ -0,0 +1,13 @@
+//package 100 Days of code.Day30;
+
+public class Day30 {
+    public static void main(String[] args) {
+        //Sorting an String Arrays
+        String arr[]={"Dev","Shiv","Khana","Ganesh","Krishna","Hanuman","Ram"};
+        java.util.Arrays.sort(arr);
+        for (String x : arr) {
+            System.out.println(x);
+            
+        }
+    }
+}
