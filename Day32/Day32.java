e7a5ca4983aa0d4bca44aff68623a33b56c85744
Devmangrani <devmangrani5@gmail.com>
2023-05-27 19:10:11 +0530

    Day31 commit

---
+++ Day32/Day32.java
@@ -0,0 +1,26 @@
+import java.util.*;
+//package 100 Days of code.Day32;
+
+public class Day32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        int choice;
+        int evensum = 0;
+        int oddsum = 0;
+        do{
+            System.out.println("Enter no. : ");
+             int number = sc.nextInt();
+             if (number % 2 == 0) {
+                evensum += number;
+             }else {
+                oddsum += number;
+             }
+             System.out.println("Do you want to continue? press 1 for yess or 0 or no");
+             choice = sc.nextInt();
+        }while(choice == 1);
+        System.out.println("Sum of even no. : " + evensum);
+        System.out.println("Sum of odd number : " + oddsum);
+    }
+}
