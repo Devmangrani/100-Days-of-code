8cd083e0b5932a29bdb8bcea948546534f4fe1b9
Devmangrani <devmangrani5@gmail.com>
2023-06-01 22:23:38 +0530

    Day36 commit

---
+++ Day36/Day36.java
@@ -0,0 +1,16 @@
+//package 100 Days of code.Day36;
+
+public class Day36 {
+    //Methods Practise
+    static int min(int x, int y){
+        if(x<y)
+            return x;
+        else
+            return y;
+        
+    }
+    public static void main(String[] args) {
+        int a=10,b=19,c;
+        System.out.println(min(a,b));
+    }
+}
