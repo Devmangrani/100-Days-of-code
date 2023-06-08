cc63d2104e5c1da343b015df1d86f13cc9b8801b
Devmangrani <devmangrani5@gmail.com>
2023-06-08 17:08:21 +0530

    Dayb 42 commit

---
+++ Day41/Day-41.java
@@ -0,0 +1,26 @@
+public class Day-41 {
+    //Method Overloading
+    public static int max(int x, int y) {
+        return x>y?x:y;
+    }
+    public static float max(float x, float y) {
+        if(x>y)
+        return x;
+        else
+        return y;
+    }
+    public static int max(int x, int y, int z) {
+        if(x>y && x>z)
+        return x;
+        else if (y>z)
+        return y;
+        else 
+        return z;
+    }
+    public static void main(String[] args) {
+        System.out.println(max(10, 4));
+        System.out.println(max(15.4f, 10.6f));
+        System.out.println(max(10, 5, 18));
+    }
+}
+}
