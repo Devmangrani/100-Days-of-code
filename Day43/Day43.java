c8d03a09eb1339ff22ba41924efb75a2e2ea640b
Devmangrani <devmangrani5@gmail.com>
2023-06-09 23:28:51 +0530

    Day 42 commit

---
+++ Day43/Day43.java
@@ -0,0 +1,18 @@
+//package 100 Days of code.Day43;
+
+public class Day43 {
+    //Maximum of numbers Using Varangs
+    public static int max(int A[]) {
+        if(A.lenght==0)return Integer.MIN_VALUE;
+        int max = A[0];
+        
+    }
+    public static void main(String[] args) {
+        int n=5;
+        if(n>0){
+            System.out.println(n);
+            max(10,30);
+        }
+        
+    }
+}
