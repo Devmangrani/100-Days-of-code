5d2e1e2f58ec2d0c8a8cb2ccf75001ade5271016
Devmangrani <devmangrani5@gmail.com>
2023-06-13 23:08:19 +0530

    Day47 committed

---
+++ Day47/Day47.java
@@ -0,0 +1,35 @@
+public class Day47 {
+    public class Methods {
+        // Find a Number is Prime
+        /*
+         * static boolean isPrime(int n) {
+         * 
+         * for (int i = 2; i < n / 2; i++) {
+         * if (n % i == 0) {
+         * return false;
+         * }
+         * return true;
+         * }
+         * 
+         * }
+         */
+        /*public static int HCF(int m, int n) {
+            while (m != n) {
+                if (m > n)
+                    m = m - n;
+                else
+                    n = n - m;
+            }
+            return n;
+        }*/
+        
+        public static void main(String[] args) {
+            // Find Greatest Common Divisor(GCD)
+            // FInd Highest Common Fcator (HCF)
+            System.out.println(HCF(35, 55));
+            //Find Maximum Elements in an Array
+            //Method Overloading
+        }
+    }
+    
+}
