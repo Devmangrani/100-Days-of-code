1694467a1c5008f646083b46d8cfc25297a0401a
Devmangrani <devmangrani5@gmail.com>
2023-06-14 23:49:55 +0530

    Day48 commit

---
+++ Day48/Day48.java
@@ -0,0 +1,25 @@
+//package 100 Days of code.Day48;
+
+public class Day48 {
+    class Rectangle 
+    //Object And Classes
+{ 
+ public int length; 
+ public int breadth; 
+ public int area() 
+ { 
+ return length*breadth; 
+ } 
+ public int perimeter () 
+ { 
+ return 2*(length+breadth); 
+ } 
+class test 
+{ 
+ public static void main(String args[]) 
+ { 
+ Rectangle r=new Rectangle(); 
+ } 
+}
+
+}}
