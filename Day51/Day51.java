12671fa1c536ebf0e16f632ec9c0c3c250dc61d3
Devmangrani <devmangrani5@gmail.com>
2023-06-17 15:20:29 +0530

    Day51 commit

---
+++ Day51/Day51.java
@@ -0,0 +1,86 @@
+//package 100 Days of code.Day51;
+class Cylinder{
+    private double radius;
+    private double height;
+    public double getRadius(){
+        return radius;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public double seRradius( double r){
+        return radius = r;
+    }
+    public double setHeight(double h){
+        return height =h;
+    }
+    public double setDimensuions(int r, int h){
+        return radius;
+        //return height;
+    }
+}
+class Product{
+    private String itemno;
+    private String name;
+    private double price;
+    private short Quantity;
+    public String getItemno(){
+        return itemno;
+    } 
+    public double getPrice(){
+        return price;
+    } 
+    public short getQuantity(){
+        return Quantity;
+    } 
+    public String getName(){
+        return name;
+    } 
+   /*  public String setItemno(String i){
+        return itemno = i;
+    }*/
+    public double setprice(Double p){
+        return price = p;
+    }
+    public short setQuantity(Short q){
+        return Quantity = q;
+    }
+    public String setName(String n){
+        return name = n;
+    }
+}
+class Customer{
+    private int custid;
+    private String name;
+    private String address;
+    private int phoneno;
+    public int getCustld(){
+        return custid;
+    } 
+    public int getPhoneno(){
+        return phoneno;
+    } 
+    public String getAdress(){
+        return address;
+    } 
+    public String getName(){
+        return name;
+    } 
+    /*public int setCustid(int c){
+        return custid = c;
+    }*/
+    public int setPhoneno(int p){
+        return phoneno = p;
+    }
+    public String setAdress(String a){
+        return address = a;
+    }
+    /*public String setName(String n){
+        return name = n;
+    }*/
+}
+public class Day51 {
+    public static void main(String[] args) {
+        
+    }
+}
