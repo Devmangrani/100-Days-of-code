1a58b6c64544e7721c558c38a8b66f6bb6031c61
Devmangrani <devmangrani5@gmail.com>
2023-07-05 23:22:13 +0530

    Day 68 commit

---
+++ Day68/Day68.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Day68 {
+    public static void main(String[] args) {
+        
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your No. : ");
+            int x = sc.nextInt();
+            if (x>=0) {
+            System.out.println("Your no. is Positive");
+            }else{
+            System.out.println("Your No. is Negative");
+            }
+            
+           
+          
+            
+            double temp = 103.5;
+            if (temp > 100) {
+            System.out.println("You have fever");
+            }else{
+            System.out.println("You  Don't have fever");
+            }
+            
+    }
+    
+}
