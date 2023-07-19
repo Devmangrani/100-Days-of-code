b6da1e86dd96509197fa87a236401f09fd81aea1
Devmangrani <devmangrani5@gmail.com>
2023-07-19 21:45:24 +0530

    Day71 commit

--- Day74/Day74.java
+++ Day74/Day74.java
@@ -1,29 +1,29 @@
 public class Day74 {
     //2D Arrays ->>Spiral Matrix
-    public static void ArraysSpiral(int array[][]) {
+    public static void ArraysSpiral(int matrix[][]) {
         int startRow = 0;
         int startCol = 0;
-        int endRow = array.length-1;
-        int endCol = array[0].length-1;
+        int endRow = matrix.length-1;
+        int endCol = matrix[0].length-1;
 
         while(startRow <= endRow && startCol <= endCol){
             for (int i = startRow; i <= endRow; i++) {
-                System.out.print(array[startRow][i] + " ");
+                System.out.print(matrix[startRow][i] + " ");
             }//top
             for (int j = startRow+1; j <=endRow; j++) {
-                System.out.print(array[j][endCol] + " ");
+                System.out.print(matrix[j][endCol] + " ");
             }//right
-            for (int i = endCol-1 ; i <=startCol; i--) {
+            for (int i = endCol-1 ; i >=startCol; i--) {
                 if(startRow == endRow){
                     break;
                 }
-                System.out.print(array[endRow][i] + " ");
+                System.out.print(matrix[endRow][i] + " ");
             }//bottom
-            for (int j = endRow-1; j <=startRow+1; j--) {
+            for (int j = endRow-1; j >=startRow+1; j--) {
                 if(startCol == endCol){
                     break;
                 }
-                System.out.print(array[j][startCol] + " ");
+                System.out.print(matrix[j][startCol] + " ");
             }//left
             startCol++;
             startRow++;
@@ -33,11 +33,11 @@ public class Day74 {
         System.out.println();
     }
     public static void main(String[] args) {
-        int array[][]= {{1,2,3,4},
+        int matrix[][]= {{1,2,3,4},
                         {5,6,7,8},
                         {9,10,11,12},
                         {13,14,15,16}};
-                        ArraysSpiral(array);
+                        ArraysSpiral(matrix);
     
     }
 }
