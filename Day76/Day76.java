5e0c9e22050a43bef086ee0d0ca1906df1a9d326
Devmangrani <devmangrani5@gmail.com>
2023-07-19 22:26:52 +0530

    Day 76 commit

---
+++ Day76/Day76.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Day76 {
+    /*Task
+Given an integer,n , perform the following conditional actions:
+
+If n is odd, print Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Complete the stub code provided in your editor to print whether or not n is weird. */
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    if(n%2!=0){
+        System.out.println("Weird");
+    }else{
+        if(n>2&&n>5){
+            System.out.println("Not Weird");
+        }else if(n>6 && n<20){
+            System.out.println("Weird");
+        }else{
+            System.out.println("Not Weird");
+        }
+    }
+}
+}
