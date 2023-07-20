2eb8345ef72bb3d0e4f7bb78902bba08bd2d2872
Devmangrani <devmangrani5@gmail.com>
2023-07-20 22:44:54 +0530

    Day77 commit

--- Day76/Day76.java
+++ Day76/Day76.java
@@ -9,7 +9,7 @@ If  is even and in the inclusive range of 2 to 5, print Not Weird
 If n is even and in the inclusive range of 6 to 20, print Weird
 If n is even and greater than 20, print Not Weird
 Complete the stub code provided in your editor to print whether or not n is weird. */
-public static void main(String[] args) {
+/*public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     if(n%2!=0){
@@ -23,5 +23,28 @@ public static void main(String[] args) {
             System.out.println("Not Weird");
         }
     }
+}*/
+/*Sample Input
+
+42
+3.1415
+Welcome to HackerRank's Java tutorials!
+
+Sample Output
+
+String: Welcome to HackerRank's Java tutorials!
+Double: 3.1415
+Int: 42 */
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int i = sc.nextInt();
+    double d = sc.nextDouble();
+    sc.nextLine();//Single word Entry Without any delimeters
+    String s = sc.nextLine();
+  //  s = sc.nextLine();
+    System.out.println("String :" + s);
+    System.out.println("Double :" + d);
+    System.out.println("Int :" + i);
+    sc.close();
 }
 }
---
+++ Day77/Day77.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Day77 {
+    /*
+     * Sample Input
+     * 
+     * 2
+     * Sample Output
+     * 
+     * 2 x 1 = 2
+     * 2 x 2 = 4
+     * 2 x 3 = 6
+     * 2 x 4 = 8
+     * 2 x 5 = 10
+     * 2 x 6 = 12
+     * 2 x 7 = 14
+     * 2 x 8 = 16
+     * 2 x 9 = 18
+     * 2 x 10 = 20
+     */
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + i*n);
+        }
+    }*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            System.out.println((n + ));
+        }
+    }
+}
