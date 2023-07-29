f359e9711e06b6677326011e8807d331a6860ec0
Devmangrani <devmangrani5@gmail.com>
2023-07-29 23:52:30 +0530

    Day83

--- Day83/Day83.java
+++ Day83/Day83.java
@@ -1,7 +1,27 @@
 //package 100 Days of code.Day83;
 
 public class Day83 {
-    //Pairs in Array
+    
+    public static int trappedwater(int height[]) {
+        int n = height.length;
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for (int i=1; i<n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+        int rightMax[] = new int[n];
+        rightMax[n-1] = height[n-1];
+        for (int i=n-2; i>=0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+
+        }
+        int trappedwater = 0;
+        for (int i=0; i<n; i++) {
+            int waterlevel = Math.min(leftMax[i],rightMax[i]);
+            trappedwater += waterlevel -height[i];
+        }
+        return trappedwater;
+    }
     public static void main(String[] args) {
         
     }
