834ee229a0dc79eaca58afe51ec5e98cd20eafd5
Devmangrani <devmangrani5@gmail.com>
2023-09-12 22:38:13 +0530

    Day 100

---
+++ 100Day/Day100.java
@@ -0,0 +1,34 @@
+//package 100 Days of code.100Day;
+//410. Split Array Largest Sum
+
+public class Day100 {
+    class Solution {
+        public int splitArray(int[] nums, int k) {
+            int start = 0;
+            int end = 0;
+            for( int  i = 0; i<nums.length ; i++){
+              start = Math.max(start , nums[i]);
+              end += nums[i];
+            }
+            while(start<end){
+              int mid = (start+end)/2;
+              int sum = 0;
+               int pieces = 1;
+               for (int num : nums) {
+                 if(sum + num>mid){
+                   sum = num;
+                   pieces++;
+                 }else{
+                   sum += num;  
+                 }
+               }
+               if(pieces > k){
+                 start = mid +1;
+               }else {
+                 end = mid;
+               }
+            }
+            return end; 
+        }
+    }
+}
